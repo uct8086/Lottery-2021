0a87da79270798fc73c30c748ff7f5fb1fc9f27c
kbzero <belieftang@163.com>
2021-11-19 09:41:41 +0800

    feat: add echarts

--- client/modules/home/index.vue
+++ client/modules/home/index.vue
@@ -5,15 +5,18 @@
         跳转到详情页
       </button>
     </div>
-    <h1>欢迎支持:</h1>
-    <img
-      src="../../assets/image/afb.jpg"
-      alt="empty image"
-    >
-    <img
-      src="../../assets/image/weixin.png"
-      alt="empty image"
-    >
+    <div class="chart-zone">
+      <div
+        id="chart-one"
+        class="chart-content"
+      />
+    </div>
   </div>
 </template>
 <script src="./script.js"></script>
+<style lang="less">
+.chart-content{
+  width: 100%;
+  height: 300px;
+}
+</style>
--- client/modules/home/script.js
+++ client/modules/home/script.js
@@ -2,6 +2,8 @@ import HttpHelper from "common/utils/axios_helper.js";
 import { reactive, onMounted } from 'vue';
 import { useRouter } from 'vue-router';
 import { FETCH_DATA } from "../../common/urls";
+import * as echarts from 'echarts';
+
 export default {
     setup() {
         const data = reactive({
@@ -14,8 +16,26 @@ export default {
         };
 
         onMounted(() => {
-            d3.select('body');
             getData();
+            // 基于准备好的dom，初始化echarts实例
+            var myChart = echarts.init(document.getElementById('chart-one'));
+            // 绘制图表
+            myChart.setOption({
+                title: {
+                    text: 'Static'
+                },
+                tooltip: {},
+                xAxis: {
+                    data: ['衬衫', '羊毛衫', '雪纺衫', '裤子', '高跟鞋', '袜子']
+                },
+                yAxis: {},
+                series: [{
+                    name: '销量',
+                    type: 'bar',
+                    data: [5, 20, 36, 10, 10, 20]
+                }]
+            });
+
         });
 
         const toDetail = () => {
@@ -24,7 +44,6 @@ export default {
 
         return {
             data,
-            getData,
             toDetail
         };
     }
