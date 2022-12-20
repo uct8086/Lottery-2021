ce8ac8f8b0c7e306a56b4e5262ac0b44f334aecb
uct8086 <belieftang@163.com>
2022-12-20 16:45:53 +0800

    feat: add rul

--- client/common/urls/index.js
+++ client/common/urls/index.js
@@ -1,7 +1,9 @@
+const prefix = 'http://124.222.27.117/';
+
 module.exports = {
-    FETCH_DATA: 'api/fetch_data',
-    UPDATE_ORIGIN_DATA: 'api/update_origin_data',
-    SEARCH_BY_PARAMS: 'api/search_by_params',
-    FETCH_TOTAL_INFO: 'api/fetch_total_info',
-    FETCH_HOME_DETAIL: 'api/fetch_home_detail'
+    FETCH_DATA: `${prefix}api/fetch_data`,
+    UPDATE_ORIGIN_DATA: `${prefix}api/update_origin_data`,
+    SEARCH_BY_PARAMS: `${prefix}api/search_by_params`,
+    FETCH_TOTAL_INFO: `${prefix}api/fetch_total_info`,
+    FETCH_HOME_DETAIL: `${prefix}api/fetch_home_detai`
 };
