050e5b1a1e08232c475278166a36568b00b72102
uct8086 <belieftang@163.com>
2023-01-03 14:36:35 +0800

    fix: start

--- server/getLotteryData.js
+++ server/getLotteryData.js
@@ -4,47 +4,49 @@ class ForkLottery {
 
     async start() {
         let page = 1, totalPages = 10000, data = [];
-
-        do {
-            let url = `https://webapi.sporttery.cn/gateway/lottery/getHistoryPageListV1.qry?gameNo=85&provinceId=0&pageSize=30&isVerify=1&pageNo=${page}`;
-            let { pages, list } = await new Promise((resolve, reject) => {
-                request(url, (error, response, body) => {
-                    if (error) {
-                        return reject(error);
-                    }
-                    if (response && response.statusCode === 200) {
-                        let temp = JSON.parse(body);
-                        const { pages, list } = temp.value;
-                        return resolve({ pages, list });
-                    }
-                });
-            });
-            if (!list) {
-                console.log(page);
-                return;
-            } else {
-                totalPages = pages;
-                console.log(page, totalPages);
-            }
-            for (let item of list) {
-                let { lotteryDrawNum,
-                    lotteryDrawResult,
-                    lotteryDrawTime } = item;
-                data.push({
-                    lotteryDrawNum,
-                    lotteryDrawResult,
-                    lotteryDrawTime,
-                });
-            }
-            page++;
-        } while (page <= totalPages);
         try {
+            do {
+                let url = `https://webapi.sporttery.cn/gateway/lottery/getHistoryPageListV1.qry?gameNo=85&provinceId=0&pageSize=30&isVerify=1&pageNo=${page}`;
+                let { pages, list } = await new Promise((resolve, reject) => {
+                    request(url, (error, response, body) => {
+                        if (error) {
+                            return reject(error);
+                        }
+                        if (response && response.statusCode === 200) {
+                            let temp = JSON.parse(body);
+                            const { pages, list } = temp.value;
+                            return resolve({ pages, list });
+                        }
+                    });
+                });
+                if (!list) {
+                    console.log(page);
+                    return;
+                } else {
+                    totalPages = pages;
+                    console.log(page, totalPages);
+                }
+                for (let item of list) {
+                    let { lotteryDrawNum,
+                        lotteryDrawResult,
+                        lotteryDrawTime } = item;
+                    data.push({
+                        lotteryDrawNum,
+                        lotteryDrawResult,
+                        lotteryDrawTime,
+                    });
+                }
+                page++;
+            } while (page <= totalPages);
             const dataPath = `./server/data.json`;
             const exist = fs.existsSync(dataPath);
+            console.log('exist: ', exist);
             if (exist) {
                 fs.rmSync(dataPath);
             }
+            console.log('remove success. ');
             fs.writeFileSync(dataPath, JSON.stringify(data));
+            console.log('write success. ');
         } catch (e) {
             console.log(`got lottery data error is : ${e && e.message || ''}`);
         }
